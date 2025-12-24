56e078008181fa54d680cc3754dffd9065325ca7
的梦 <29492511@qq.com>
2025-12-24 08:39:59 +0800

    2025-12-24 08:39:56

--- src/q2.js
+++ src/q2.js
@@ -8,7 +8,7 @@ const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
 const os = require("os");
-const trash = require("trash");
+// const trash = require("trash"); // trash 7.x is ESM only, use dynamic import instead
 
 // ==================== 从 qqq.js 导入核心接口 ====================
 const qqq = require("./qqq");
