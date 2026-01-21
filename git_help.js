e3b702b78024982ac96fcdf638838bd5aaa474b9
的梦 <29492511@qq.com>
2026-01-21 16:28:24 +0800

    chore: v15.47.1 (q3) - 拨乱反正：锁定 ffmpeg 1.1.0 包装版本，移除 package.json 冗余依赖

--- git_help.js
+++ git_help.js
@@ -207,6 +207,7 @@ class LocalAIReleaseAssistant {
                         'win32-ia32': 'win32-ia32',
                         'win32-arm64': 'win32-x64', // win32-arm64 暂用 x64 兼容版
                         'linux-x64': 'linux-x64',
+                        'linux-arm64': 'linux-arm64',
                         'darwin-x64': 'darwin-x64',
                         'darwin-arm64': 'darwin-arm64'
                     };
